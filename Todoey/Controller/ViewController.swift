3a95260ce1630c5893e390b30aece8fdc3b0308b
bgs <mohamad.15@students.amikom.ac.id>
2023-08-14 11:46:09 +0700

    save use userdefaults

--- Todoey/Controller/ViewController.swift
+++ Todoey/Controller/ViewController.swift
@@ -20,54 +20,69 @@ class ViewController: UITableViewController {
         newItem.title = "Find Mike"
         itemArray.append(newItem)
         
+        let newItem2 = Item()
+        newItem2.title = "Find Mike"
+        itemArray.append(newItem2)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+            itemArray = items
+        }
+        
     }
     
     //MARK: TableViewDataSource
+    //when you run tableView.reload you will call this function again
+    
+    //get amount of data
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
         
     }
     
+    //display data from model to cell
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //create reusable cell
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row].title
+        //initialize item
+        let item = itemArray[indexPath.row]
+        //set textlabel to item.title
+        cell.textLabel?.text = item.title
+        //check mark
+        cell.accessoryType = item.done ? .checkmark : .none
+        
         return cell
     }
     
     //MARK: TableView Delegate Method
+    //func when cell click
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        //add boolean
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        //add animation
         tableView.deselectRow(at: indexPath, animated: true)
-        
+        tableView.reloadData()
     }
     
-    
-    @IBAction func buttonNext(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "AnimationView", sender: self)
-    }
     
     //MARK: Add New Item
     
     @IBAction func addButtonPress(_ sender: UIBarButtonItem) {
+        //initialize UITextField
         var textField = UITextField()
-        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        //initialize alert
+        let alert = UIAlertController(title: "Tambahkan Item Baru", message: "", preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Add Item", style: .default) { action in
-            print(textField.text!)
+        //action when click
+        let action = UIAlertAction(title: "Tambah", style: .default) { action in
             
             let newItem = Item()
             newItem.title = textField.text!
             self.itemArray.append(newItem)
             
-            //save to userdefaults
+            //encode
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
+            //reload table
             self.tableView.reloadData()
         }
         
